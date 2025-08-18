82fc010309ac81839de02481f1712891014dac0b
Plamen Stoilkov <sdplamen@gmail.com>
2025-08-18 19:18:34 +0300

    Update app urls

--- roman_to_digit/urls.py
+++ roman_to_digit/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from roman_digit import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('roman_digit.urls')),
-    path('', views.index, name='index'),
 
 ]
